e055fc73d9f65e15500c6b284300c0a4a4ffab54
Charles Woodring <charles.s.woodring@gmail.com>
2019-02-22 16:53:39 -0600

    first commit

---
+++ src/scripts/instructions.js
@@ -0,0 +1,13 @@
+/*
+She tries to travel to three destinations per month
+
+Then she takes a week off before starting the whole cycle again
+
+She can add points of interests for the next set of three places she will be visiting
+
+She would like to be able to add a new point of interest in one of her destinations so she can have a list of places she needs to visit when she travels
+
+She has asked to be able to delete points of interest from her list. But to ensure she does not accidentally delete anything, she would like an alert to confirm the delete action
+
+Mira expects an application that has a clean user interface with easy to read text and an intuitive interface. However, it is much more important that the application performs the required functionality than that it looks perfect
+*/
